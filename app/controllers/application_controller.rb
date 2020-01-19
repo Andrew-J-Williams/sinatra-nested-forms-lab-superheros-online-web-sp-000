b83383038ac9117b511dc101fde15d2bae788751
Andrew Williams <1williamsaj@gmail.com>
2020-01-19 23:56:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,9 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(params[:team])
 
-      params[:team][:members].each do   
+      params[:team][:members].each do |details|
+
+      end  
 
       erb :team
     end
